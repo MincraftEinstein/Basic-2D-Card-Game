b4a5cff2f8ef7b25401054b4eb66d312b8670114
Seth Van Meter <SethVM0178@gmail.com>
2021-01-05 08:37:47 -0500

    Initial commit

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DrawCards : NetworkBehaviour
+{
+    public PlayerManager PlayerManager;
+
+    public void onClick()
+    {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
+    }
+}
